2a84468f0e43ae5fe046fe300b31a3650365968a
sabihDev <8203sabi.rahman@gmail.com>
2023-08-31 05:38:54 -0700

    error is not fixed

--- js/script.js
+++ js/script.js
@@ -170,160 +170,7 @@ formationButtons.forEach(element => {
             formationForm.style.display = 'none';
         }, 4000);
     }
-    var playerAdditionButtons = document.querySelectorAll('.add-player-btn');
-    playerAdditionButtons.forEach(playerAdditionButton => {
-        playerAdditionButton.onclick = () => {
-            Hide(playerAdditionButton);
-            console.log(playerAdditionButtons);
-            submitPlayerPropsButton.onclick = () => {
-                Show(playersAdditionForm);
-                function PlayerAdded() {
-                    let player = new Player(
-                        playerAdditionButton,
-                        playerInputs.inputPlayerName.value,
-                        playerInputs.inputPlayerHeight.value,
-                        playerInputs.inputPlayerDef.value,
-                        playerInputs.inputPlayerPhy.value,
-                        playerInputs.inputPlayerDrrible.value,
-                        playerInputs.inputPlayerPace.value,
-                        playerInputs.inputPlayerMainFoot.value,
-                    );
-                    console.log(playerInputs.inputPlayerHeight.value);
-                    console.log(playerInputs.inputPlayerDef.value);
-                }
 
-                function PlayerAddedNot() {
-                    alert(`Form is not correctly filled.
-                    Make sure you would add text in name and foot of a player
-                    and add numbers in the remaining inputs.`);
-                }
-
-                function CheckAllNumeric() {
-                    if (
-                        (ValidateInteger(playerInputs.inputPlayerDef.value)) &&
-                        (ValidateInteger(playerInputs.inputPlayerDrrible.value)) &&
-                        (ValidateInteger(playerInputs.inputPlayerPhy.value)) &&
-                        (ValidateInteger(playerInputs.inputPlayerPace.value)) &&
-                        (ValidateInteger(playerInputs.inputPlayerHeight.value))) {
-                        alert('Your Registration number has accepted....');
-                        PlayerAdded();
-                    }
-                    else {
-                        alert('Please input numeric characters only for height, physcique, defence, drribles and pace');
-                        PlayerAddedNot();
-                    }
-                }
-
-                CheckAllNumeric();
-            };
-        }
-    });
-
-    var pictures = document.querySelectorAll(".picture");
-    pictures.forEach(editButton => {
-        editButton.onclick = () => {
-
-            let playerProperties = {
-                playerNameProp: editButton.parentElement.childNodes[5].childNodes[1],
-                playerHeightProp: editButton.parentElement.childNodes[7].childNodes[1].childNodes[1],
-                playerPhyProp: editButton.parentElement.childNodes[7].childNodes[3].childNodes[1],
-                playerDefProp: editButton.parentElement.childNodes[7].childNodes[5].childNodes[1],
-                playerDrribleProp: editButton.parentElement.childNodes[7].childNodes[7].childNodes[1],
-                playerPaceProp: editButton.parentElement.childNodes[7].childNodes[9].childNodes[1],
-                playerMainFootProp: editButton.parentElement.childNodes[7].childNodes[11].childNodes[1],
-            }
-
-            Show(playersAdditionForm);
-            playerInputs.inputPlayerName.value = "Name: " + playerProperties.playerNameProp.innerHTML;
-            playerInputs.inputPlayerHeight.placeholder = "Height: " + playerProperties.playerHeightProp.innerHTML;
-            playerInputs.inputPlayerPhy.placeholder = "Physcique: " + playerProperties.playerPhyProp.innerHTML;
-            playerInputs.inputPlayerDef.placeholder = "Defence: " + playerProperties.playerDefProp.innerHTML;
-            playerInputs.inputPlayerDrrible.placeholder = "Drribles: " + playerProperties.playerDrribleProp.innerHTML;
-            playerInputs.inputPlayerPace.placeholder = "Pace: " + playerProperties.playerPaceProp.innerHTML;
-            playerInputs.inputPlayerMainFoot.value = "Main foot: " + playerProperties.playerMainFootProp.innerHTML;
-
-            playerInputs.inputPlayerName.onfocus = () => {
-                playerInputs.inputPlayerName.value = playerProperties.playerNameProp.innerHTML;
-            }
-
-            playerInputs.inputPlayerHeight.onfocus = () => {
-                playerInputs.inputPlayerHeight.value = playerProperties.playerHeightProp.innerHTML;
-            }
-
-            playerInputs.inputPlayerPhy.onfocus = () => {
-                playerInputs.inputPlayerPhy.value = playerProperties.playerPhyProp.innerHTML;
-            }
-
-            playerInputs.inputPlayerDef.onfocus = () => {
-                playerInputs.inputPlayerDef.value = playerProperties.playerDefProp.innerHTML;
-            }
-
-            playerInputs.inputPlayerDrrible.onfocus = () => {
-                playerInputs.inputPlayerDrrible.value = playerProperties.playerDrribleProp.innerHTML;
-            }
-
-            playerInputs.inputPlayerPace.onfocus = () => {
-                playerInputs.inputPlayerPace.value = playerProperties.playerPaceProp.innerHTML;
-            }
-
-            playerInputs.inputPlayerMainFoot.onfocus = () => {
-                playerInputs.inputPlayerMainFoot.value = playerProperties.playerMainFootProp.innerHTML;
-            }
-            submitPlayerPropsButton.onclick = () => {
-
-                function GetTeamName() {
-                    if (GetParentInHierarchy(4, playerProperties.playerNameProp) == teamAPlayersContainer) {
-                        return teamAName.innerHTML;
-                    }
-                    else {
-                        return teamBName.innerHTML;
-                    }
-                }
-
-                const jsonData = {
-                    id: Math.floor(Math.random() * 100000),
-                    TeamName: GetTeamName(),
-                    name: playerProperties.playerNameProp.innerHTML,
-                    height: playerProperties.playerHeightProp.innerHTML,
-                    phy: playerProperties.playerPhyProp.innerHTML,
-                    def: playerProperties.playerDefProp.innerHTML,
-                    drrible: playerProperties.playerDrribleProp.innerHTML,
-                    pace: playerProperties.playerPaceProp.innerHTML,
-                    mainFoot: playerProperties.playerMainFootProp.innerHTML,
-                };
-
-                const playerId = jsonData.id;
-                let newHeight = playerInputs.inputPlayerHeight.value;
-                let newName = playerInputs.inputPlayerName.value;
-                let newDef = playerInputs.inputPlayerDef.value;
-                let newPhy = playerInputs.inputPlayerPhy.value;
-                let newDrrible = playerInputs.inputPlayerDrrible.value;
-                let newPace = playerInputs.inputPlayerPace.value;
-                let newMainFoot = playerInputs.inputPlayerMainFoot.value;
-
-                playerProperties.playerNameProp = newName;
-                playerProperties.playerHeightProp = newHeight;
-                playerProperties.playerPhyProp = newPhy;
-                playerProperties.playerDefProp = newDef;
-                playerProperties.playerDrribleProp = newDrrible;
-                playerProperties.playerPaceProp = newPace;
-                playerProperties.playerMainFootProp = newMainFoot;
-
-                Hide(playersAdditionForm);
-                if (playerId == jsonData.id) {
-                    jsonData.height = newHeight;
-                    jsonData.name = newName;
-                    jsonData.phy = newPhy;
-                    jsonData.def = newDef;
-                    jsonData.drrible = newDrrible;
-                    jsonData.pace = newPace;
-                    jsonData.mainFoot = newMainFoot;
-                    let jsonString = JSON.stringify(jsonData);
-                    players = JSON.parse(JSON.stringify(jsonString));
-                }
-            }
-        }
-    });
 });
 
 function HandleFormationSubmission(parents) {
@@ -387,53 +234,7 @@ function CreatePlayerDiv() {
     `;
     playerDiv.style.background = 'white';
 
-    playerAdditionButtons.forEach(playerAdditionButton => {
-        playerAdditionButton.onclick = () => {
-            Hide(playerAdditionButton);
-            submitPlayerPropsButton.onclick = () => {
-                Show(playersAdditionForm);
-                function PlayerAdded() {
-                    let player = new Player(
-                        playerAdditionButton,
-                        playerInputs.inputPlayerName.value,
-                        playerInputs.inputPlayerHeight.value,
-                        playerInputs.inputPlayerDef.value,
-                        playerInputs.inputPlayerPhy.value,
-                        playerInputs.inputPlayerDrrible.value,
-                        playerInputs.inputPlayerPace.value,
-                        playerInputs.inputPlayerMainFoot.value,
-                    );
-                    console.log(playerInputs.inputPlayerHeight.value);
-                    console.log(playerInputs.inputPlayerDef.value);
-                }
 
-                function PlayerAddedNot() {
-                    alert(`Form is not correctly filled.
-                    Make sure you would add text in name and foot of a player
-                    and add numbers in the remaining inputs.`);
-                }
-
-                function CheckAllNumeric() {
-                    if (
-                        (ValidateInteger(playerInputs.inputPlayerDef.value)) &&
-                        (ValidateInteger(playerInputs.inputPlayerDrrible.value)) &&
-                        (ValidateInteger(playerInputs.inputPlayerPhy.value)) &&
-                        (ValidateInteger(playerInputs.inputPlayerPace.value)) &&
-                        (ValidateInteger(playerInputs.inputPlayerHeight.value))) {
-                        alert('Your Registration number has accepted....');
-                        PlayerAdded();
-                    }
-                    else {
-                        alert('Please input numeric characters only for height, physcique, defence, drribles and pace');
-                        PlayerAddedNot();
-                    }
-                }
-
-                CheckAllNumeric();
-                Hide(playersAdditionForm);
-            };
-        }
-    });
 
     return playerDiv;
 }
@@ -478,4 +279,158 @@ buttons.forEach((button) => {
             Hide(tooltip);
         });
     });
+});
+
+playerAdditionButtons.forEach(playerAdditionButton => {
+    playerAdditionButton.onclick = () => {
+
+        Hide(playerAdditionButton);
+        submitPlayerPropsButton.onclick = () => {
+            Show(playersAdditionForm);
+            function PlayerAdded() {
+                console.log(playerAdditionButtons);
+
+                let player = new Player(
+                    playerAdditionButton,
+                    playerInputs.inputPlayerName.value,
+                    playerInputs.inputPlayerHeight.value,
+                    playerInputs.inputPlayerDef.value,
+                    playerInputs.inputPlayerPhy.value,
+                    playerInputs.inputPlayerDrrible.value,
+                    playerInputs.inputPlayerPace.value,
+                    playerInputs.inputPlayerMainFoot.value,
+                );
+            }
+
+            function PlayerAddedNot() {
+                alert(`Form is not correctly filled.
+                Make sure you would add text in name and foot of a player
+                and add numbers in the remaining inputs.`);
+            }
+
+            function CheckAllNumeric() {
+                if (
+                    (ValidateInteger(playerInputs.inputPlayerDef.value)) &&
+                    (ValidateInteger(playerInputs.inputPlayerDrrible.value)) &&
+                    (ValidateInteger(playerInputs.inputPlayerPhy.value)) &&
+                    (ValidateInteger(playerInputs.inputPlayerPace.value)) &&
+                    (ValidateInteger(playerInputs.inputPlayerHeight.value))) {
+                    alert('Your Registration number has accepted....');
+                    PlayerAdded();
+                }
+                else {
+                    alert('Please input numeric characters only for height, physcique, defence, drribles and pace');
+                    PlayerAddedNot();
+                }
+            }
+
+            CheckAllNumeric();
+        };
+    }
+});
+
+var pictures = document.querySelectorAll(".picture");
+pictures.forEach(editButton => {
+    editButton.onclick = () => {
+
+        let playerProperties = {
+            playerNameProp: editButton.parentElement.childNodes[5].childNodes[1],
+            playerHeightProp: editButton.parentElement.childNodes[7].childNodes[1].childNodes[1],
+            playerPhyProp: editButton.parentElement.childNodes[7].childNodes[3].childNodes[1],
+            playerDefProp: editButton.parentElement.childNodes[7].childNodes[5].childNodes[1],
+            playerDrribleProp: editButton.parentElement.childNodes[7].childNodes[7].childNodes[1],
+            playerPaceProp: editButton.parentElement.childNodes[7].childNodes[9].childNodes[1],
+            playerMainFootProp: editButton.parentElement.childNodes[7].childNodes[11].childNodes[1],
+        }
+
+        Show(playersAdditionForm);
+        playerInputs.inputPlayerName.value = "Name: " + playerProperties.playerNameProp.innerHTML;
+        playerInputs.inputPlayerHeight.placeholder = "Height: " + playerProperties.playerHeightProp.innerHTML;
+        playerInputs.inputPlayerPhy.placeholder = "Physcique: " + playerProperties.playerPhyProp.innerHTML;
+        playerInputs.inputPlayerDef.placeholder = "Defence: " + playerProperties.playerDefProp.innerHTML;
+        playerInputs.inputPlayerDrrible.placeholder = "Drribles: " + playerProperties.playerDrribleProp.innerHTML;
+        playerInputs.inputPlayerPace.placeholder = "Pace: " + playerProperties.playerPaceProp.innerHTML;
+        playerInputs.inputPlayerMainFoot.value = "Main foot: " + playerProperties.playerMainFootProp.innerHTML;
+
+        playerInputs.inputPlayerName.onfocus = () => {
+            playerInputs.inputPlayerName.value = playerProperties.playerNameProp.innerHTML;
+        }
+
+        playerInputs.inputPlayerHeight.onfocus = () => {
+            playerInputs.inputPlayerHeight.value = playerProperties.playerHeightProp.innerHTML;
+        }
+
+        playerInputs.inputPlayerPhy.onfocus = () => {
+            playerInputs.inputPlayerPhy.value = playerProperties.playerPhyProp.innerHTML;
+        }
+
+        playerInputs.inputPlayerDef.onfocus = () => {
+            playerInputs.inputPlayerDef.value = playerProperties.playerDefProp.innerHTML;
+        }
+
+        playerInputs.inputPlayerDrrible.onfocus = () => {
+            playerInputs.inputPlayerDrrible.value = playerProperties.playerDrribleProp.innerHTML;
+        }
+
+        playerInputs.inputPlayerPace.onfocus = () => {
+            playerInputs.inputPlayerPace.value = playerProperties.playerPaceProp.innerHTML;
+        }
+
+        playerInputs.inputPlayerMainFoot.onfocus = () => {
+            playerInputs.inputPlayerMainFoot.value = playerProperties.playerMainFootProp.innerHTML;
+        }
+        submitPlayerPropsButton.onclick = () => {
+
+            function GetTeamName() {
+                if (GetParentInHierarchy(4, playerProperties.playerNameProp) == teamAPlayersContainer) {
+                    return teamAName.innerHTML;
+                }
+                else {
+                    return teamBName.innerHTML;
+                }
+            }
+
+            const jsonData = {
+                id: Math.floor(Math.random() * 100000),
+                TeamName: GetTeamName(),
+                name: playerProperties.playerNameProp.innerHTML,
+                height: playerProperties.playerHeightProp.innerHTML,
+                phy: playerProperties.playerPhyProp.innerHTML,
+                def: playerProperties.playerDefProp.innerHTML,
+                drrible: playerProperties.playerDrribleProp.innerHTML,
+                pace: playerProperties.playerPaceProp.innerHTML,
+                mainFoot: playerProperties.playerMainFootProp.innerHTML,
+            };
+
+            const playerId = jsonData.id;
+            let newHeight = playerInputs.inputPlayerHeight.value;
+            let newName = playerInputs.inputPlayerName.value;
+            let newDef = playerInputs.inputPlayerDef.value;
+            let newPhy = playerInputs.inputPlayerPhy.value;
+            let newDrrible = playerInputs.inputPlayerDrrible.value;
+            let newPace = playerInputs.inputPlayerPace.value;
+            let newMainFoot = playerInputs.inputPlayerMainFoot.value;
+
+            playerProperties.playerNameProp = newName;
+            playerProperties.playerHeightProp = newHeight;
+            playerProperties.playerPhyProp = newPhy;
+            playerProperties.playerDefProp = newDef;
+            playerProperties.playerDrribleProp = newDrrible;
+            playerProperties.playerPaceProp = newPace;
+            playerProperties.playerMainFootProp = newMainFoot;
+
+            Hide(playersAdditionForm);
+            if (playerId == jsonData.id) {
+                jsonData.height = newHeight;
+                jsonData.name = newName;
+                jsonData.phy = newPhy;
+                jsonData.def = newDef;
+                jsonData.drrible = newDrrible;
+                jsonData.pace = newPace;
+                jsonData.mainFoot = newMainFoot;
+                let jsonString = JSON.stringify(jsonData);
+                players = JSON.parse(JSON.stringify(jsonString));
+            }
+        }
+    }
 });
